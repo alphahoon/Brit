580482e98dbb50828851d82ee18c133e7eeeb4dd
kimyoungeun <dhdepddl@gmail.com>
2017-05-20 00:42:06 +0900

    add pork cutlet to recipe

--- js/recipe.js
+++ js/recipe.js
@@ -109,4 +109,61 @@ var omelet_rice = {"title": "Omelet with Rice",
     }, ]
 };
 
-var recipes = [carbonara, omelet_rice];
+var port_cutlet = {
+  "title": "Pork cutlet",
+    "level": 2,
+    "time": 30,
+    "imageLink": "assets/images/port_cutlet.jpg",
+    "tools":[],
+    "main":[{"name": "Egg", "amount": 2, "unit": ""},
+      {"name": "Port tenderloins", "amount": 2, "unit": "pieces"},
+      {"name": "Flour", "amount": 2, "unit": "table spoons"},
+      {"name": "butter", "amount": 2, "unit": "table spoons"},
+      {"name": "Green onion", "amount": 1, "unit": "bunch"}],
+    "sub":[{"name": "Onion", "amount": 1/2, "unit": ""},
+      {"name": "Dill pickles", "amount": 1/3, "unit": "cup"},
+      {"name": "Jalapeno pepper", "amount": 1, "unit": ""}],
+    "steps": [{
+          "step": 0,
+          "text": "Place pork between two sheets of heavy plastic (resealable freezer bags work well) on a solid, level surface.",
+          "difficulty": 1,
+          "time": 7
+      }, {
+          "step": 1,
+          "text": "Place pork pieces on a plate and season both sides generously with salt and black pepper.",
+          "difficulty": 0,
+          "time": 5
+      }, {
+          "step": 2,
+          "text": "Transfer pork pieces to a bowl filled with panko bread crumbs. Press bread crumbs firmly into the meat on both sides.",
+          "difficulty": 0,
+          "time": 10
+      }, {
+          "step": 3,
+          "text": "Transfer breaded cutlets to a clean plate, cover with plastic and refrigerate for 15 minutes.",
+          "difficulty": 0,
+          "time": 5
+      }, {
+          "step": 4,
+          "text": "Melt butter in a skillet over medium heat. Stir in pickles, jalapeno pepper, and green onions; cook and stir until onions have softened, about 3 minutes.",
+          "difficulty": 0,
+          "time": 5
+      }, {
+          "step": 5,
+          "text": "Sprinkle in 1 1/2 tablespoon flour. Cook and stir for 3 minutes.",
+          "difficulty": 1,
+          "time": 4
+      }, {
+          "step": 6,
+          "text": "Slowly stir in 2 to 3 tablespoons cold milk, whisking constantly. Stir in remaining milk, Worcestershire sauce, and black pepper. Simmer on low for 5 minutes. Taste and adjust seasoning.",
+          "difficulty": 1,
+          "time": 3
+      }, {
+          "step": 7,
+          "text": "Heat half the vegetable oil in a large skillet over medium-high heat. Add 4 breaded pork cutlets and cook until pork is not longer pink inside, and the crust is well-browned, 4 to 5 minutes per side",
+          "difficulty": 0,
+          "time": 2
+      }, ]
+};
+
+var recipes = [carbonara, omelet_rice, port_cutlet];
