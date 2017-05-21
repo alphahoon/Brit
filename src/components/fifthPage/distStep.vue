597dc7b98fc143ee49978f037d8718f94448bb71
alphahoon <alphahoon@kaist.ac.kr>
2017-05-22 00:03:36 +0900

    fifth page simple layout

---
+++ src/components/fifthPage/distStep.vue
@@ -0,0 +1,45 @@
+<template>
+<tr>
+    <!--<td class="stepNumber">{{dist.step}}</td>-->
+    <td class="firstPerson" v-if="participants.length >= 1">1</td>
+    <td class="secondPerson" v-if="participants.length >= 2">2</td>
+    <td class="thirdPerson" v-if="participants.length >= 3">3</td>
+    <td class="fourthPerson" v-if="participants.length >= 4">4</td>
+</tr>
+</template>
+
+<script>
+export default {
+    props: ['participants', 'dist']
+}
+</script>
+
+<style scoped>
+table,
+tr,
+th,
+td {
+    border: 1px solid white;
+}
+
+td {
+    font-size: 1.2em;
+    padding: 10px;
+}
+
+.firstPerson {
+    background: rgb(204, 237, 214);
+}
+
+.secondPerson {
+    background: rgba(203, 224, 239);
+}
+
+.thirdPerson {
+    background: rgba(252, 230, 194);
+}
+
+.fourthPerson {
+    background: rgba(237, 205, 203);
+}
+</style>
--- src/components/fifthPage/distTable.vue
+++ src/components/fifthPage/distTable.vue
@@ -1,15 +1,61 @@
 <template>
-<li>{{ name }}</li>
+<table>
+    <tr>
+        <!--<th>Step Number</th>-->
+        <th class="firstPerson" v-if="participants.length >= 1">{{participants[0].name}}</th>
+        <th class="secondPerson" v-if="participants.length >= 2">{{participants[1].name}}</th>
+        <th class="thirdPerson" v-if="participants.length >= 3">{{participants[2].name}}</th>
+        <th class="fourthPerson" v-if="participants.length >= 4">{{participants[3].name}}</th>
+    </tr>
+    <distStep v-for="dist in distList" :participants="participants" :dist="dist"></distStep>
+</table>
 </template>
 
 <script>
+import distStep from './distStep.vue'
+
 export default {
-    props: ['name']
+    props: ['participants', 'distList'],
+    components: {
+        distStep
+    }
 }
 </script>
 
 <style scoped>
-span {
-    font-weight: bold;
+table,
+tr,
+th,
+td {
+    border: 1px solid white;
+}
+
+table {
+    table-layout: fixed;
+    width: 100%;
+}
+
+th {
+    text-align: center;
+    font-size: 1.6em;
+    font-weight: normal;
+    color: white;
+    padding: 10px;
+}
+
+.firstPerson {
+    background: #2ecc71;
+}
+
+.secondPerson {
+    background: #3498db;
+}
+
+.thirdPerson {
+    background: #f1c40f;
+}
+
+.fourthPerson {
+    background: #e74c3c;
 }
 </style>
