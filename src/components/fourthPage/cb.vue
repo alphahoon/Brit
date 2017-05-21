3881dfd9bc4ea475b12b32382951f5ef188998aa
seayeon <dltpdus321@gmail.com>
2017-05-22 02:25:23 +0900

    change

--- src/components/fourthPage/cb.vue
+++ src/components/fourthPage/cb.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <label class="btn btn-danger" style="margin: 4px;" v-on:click="checkWork">
+        <label class="btn" :class="bgColor" style="margin: 4px;" v-on:click="checkWork">
             <span v-if="isChecked" class="glyphicon glyphicon-ok"></span>
         </label>
     </div>
@@ -8,7 +8,20 @@
 
 <script>
 export default{
-    props:['participant', 'stepIdx'],
+    props:['participant', 'idx', 'stepIdx'],
+    computed: {
+      bgColor: function () {
+            if(this.idx == 0){
+                return 'btn-success'
+            } else if(this.idx == 1){
+                return 'btn-primary'
+            } else if(this.idx == 2){
+                return 'btn-warning'
+            } else if(this.idx == 3){
+                return 'btn-danger'
+            }
+        }
+    },
     data: function() {
       return  {
         isChecked: false
