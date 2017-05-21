132bf9a4c18beeae18edc9e4ab3b65fe58bc1ff1
alphahoon <alphahoon@kaist.ac.kr>
2017-05-22 00:29:45 +0900

    image button modified

--- src/components/secondPage/menuMain.vue
+++ src/components/secondPage/menuMain.vue
@@ -1,54 +1,55 @@
 <template>
-    <div style="text-align:left;">
-      <div class="panel panel-default" style="padding-left:10px">
+<div style="text-align:left;">
+    <div class="panel panel-default" style="padding-left:10px">
         <div class="panel-body">
-          <div class="row" style="width: 90%; padding-right: 10px;">
-              <h3>{{title}}</h3>
-          </div>
-          <div class="row" style="margin-bottom: 10px;">
-              <img v-bind:src=imageLink style="width: 90%">
-          </div>
-          <div class="cardContent"> <img :src=levelSrc alt="" class="levelStar">
-              <div class="timeStr">
-                  <h5><span class="glyphicon glyphicon-time"></span> {{time}} min</h5>
-              </div>
-          </div>
+            <div class="row" style="width: 90%; padding-right: 10px;">
+                <h3>{{title}}</h3>
+            </div>
+            <div class="row" style="margin-bottom: 10px;">
+                <img v-bind:src=imageLink style="width: 90%">
+            </div>
+            <div class="cardContent"> <img :src=levelSrc alt="" class="levelStar">
+                <div class="timeStr">
+                    <h5><span class="glyphicon glyphicon-time"></span> {{time}} min</h5>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
+</div>
 </template>
 
 <script>
 export default {
     props: ['title', 'imageLink', 'level', 'time'],
     computed: {
-      difficulty: function () {
-          if (this.level == 0) {
-              return 'Easy'
-          } else if (this.level == 1) {
-              return 'Medium'
-          } else if (this.level == 2) {
-              return 'Hard'
-          }
-      },
-      levelSrc: function() {
-        if (this.level == 0) {
-            return '../../../assets/level1.png'
-        } else if (this.level == 1) {
-            return '../../../assets/level2.png'
-        } else if (this.level == 2) {
-            return '../../../assets/level3.png'
+        difficulty: function () {
+            if (this.level == 0) {
+                return 'Easy'
+            } else if (this.level == 1) {
+                return 'Medium'
+            } else if (this.level == 2) {
+                return 'Hard'
+            }
+        },
+        levelSrc: function () {
+            if (this.level == 0) {
+                return 'assets/level1.png'
+            } else if (this.level == 1) {
+                return 'assets/level2.png'
+            } else if (this.level == 2) {
+                return 'assets/level3.png'
+            }
         }
-      }
     }
 }
 </script>
 
 <style scoped>
 img.levelStar {
-  height: 25px;
-  border: None;
+    height: 25px;
+    border: None;
 }
+
 div.menuMain {
     text-align: center;
 }
@@ -59,10 +60,10 @@ div.imageLink {
 
 
 h3 {
-  margin-top: 10px;
+    margin-top: 10px;
 }
 
 div.timeStr {
-  float: right;
+    float: right;
 }
 </style>
