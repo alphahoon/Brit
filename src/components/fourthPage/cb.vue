be1ed4f6644208811660182946c20c7bf109a58d
alphahoon <alphahoon@kaist.ac.kr>
2017-05-22 05:02:51 +0900

    yeah

--- src/components/fourthPage/cb.vue
+++ src/components/fourthPage/cb.vue
@@ -1,88 +1,93 @@
 <template>
-    <div class="labelClass">
-        <div class="participantName">{{participant.name}}</div>
-        <div style="margin: 0 auto;">
+<div class="labelClass">
+    <div class="participantName">{{participant.name}}</div>
+    <div class="checkboxContainer">
         <label class="btn" :class="bgColor" style="margin: 4px; padding:3px;" v-on:click="checkWork">
             <span v-if="isChecked" class="glyphicon glyphicon-ok"></span>
         </label>
-        </div>
     </div>
+</div>
 </template>
 
 <script>
-export default{
-    props:['participant', 'idx', 'stepIdx'],
+export default {
+    props: ['participant', 'idx', 'stepIdx'],
     computed: {
-      bgColor: function () {
-            if(this.idx == 0){
+        bgColor: function () {
+            if (this.idx == 0) {
                 return 'btn-success'
-            } else if(this.idx == 1){
+            } else if (this.idx == 1) {
                 return 'btn-primary'
-            } else if(this.idx == 2){
+            } else if (this.idx == 2) {
                 return 'btn-warning'
-            } else if(this.idx == 3){
+            } else if (this.idx == 3) {
                 return 'btn-danger'
             }
         }
     },
-    data: function() {
-      return  {
-        isChecked: false
-      }
+    data: function () {
+        return {
+            isChecked: false
+        }
     },
     methods: {
-        checkWork: function() {
-          this.isChecked = !this.isChecked;
-          if (this.isChecked) {
-            this.$store.commit('pushDistribution', {
-              step: this.stepIdx,
-              participant: this.idx
-            });
-          }
-          else {
-            this.$store.commit('removeDistribution', {
-              step: this.stepIdx,
-              participant: this.idx
-            });
-          }
-          console.log(this.$store.state.participant)
-           console.log(this.$store.state.distribution)
+        checkWork: function () {
+            this.isChecked = !this.isChecked;
+            if (this.isChecked) {
+                this.$store.commit('pushDistribution', {
+                    step: this.stepIdx,
+                    participant: this.idx
+                });
+            } else {
+                this.$store.commit('removeDistribution', {
+                    step: this.stepIdx,
+                    participant: this.idx
+                });
+            }
+            console.log(this.$store.state.participant)
+            console.log(this.$store.state.distribution)
         }
     }
 }
-
 </script>
 
 
 <style scoped>
 .btn-danger {
-  width: 30px;
-  height: 30px;
-}
-.btn-success {
-  width: 30px;
-  height: 30px;
-}
-.btn-primary {
-  width: 30px;
-  height: 30px;
-}
-.btn-warning {
-  width: 30px;
-  height: 30px;
+    width: 30px;
+    height: 30px;
 }
 
-.labelClass{
-    display:inline-block;
+.btn-success {
+    width: 30px;
+    height: 30px;
+}
+
+.btn-primary {
+    width: 30px;
+    height: 30px;
+}
+
+.btn-warning {
+    width: 30px;
+    height: 30px;
+}
+
+.checkboxContainer {
+    text-align: center;
+}
+
+.labelClass {
+    display: inline-block;
     padding: 0 1em;
     align-content: center;
 }
 
-.participantName{
+.participantName {
     text-align: center;
 }
 
-.glyphicon{
+.glyphicon {
     vertical-align: text-top;
 }
 </style>
